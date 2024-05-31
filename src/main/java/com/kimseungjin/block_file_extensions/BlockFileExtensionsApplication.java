70cf49daca39770ad1ed0b05eb296bd7e50a478d
김승진 <ohksj77@gmail.com>
2024-05-31 15:06:56 +0900

    init project setting

---
+++ src/main/java/com/kimseungjin/block_file_extensions/BlockFileExtensionsApplication.java
@@ -0,0 +1,12 @@
+package com.kimseungjin.block_file_extensions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockFileExtensionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockFileExtensionsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kimseungjin/block_file_extensions/BlockFileExtensionsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.kimseungjin.block_file_extensions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockFileExtensionsApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
